93476349249f0fd09e383980cc609117403ceaf9
Reinaldo/python <reinaldo_997@hotmail.com>
2025-12-18 22:32:55 -0300

    exercicios python

---
+++ exercicio 90.py
@@ -0,0 +1,11 @@
+aluno = dict()
+aluno['nome'] = str(input('Nome: '))
+aluno['media'] = float(input(f'Media de {aluno['nome']}: '))
+if aluno['media'] >= 7:
+    aluno ['Situação'] = 'Aprovado'
+elif 5 <= aluno['media'] < 7:
+    aluno['Situação'] = 'Recuperação'
+else:
+    aluno['Situação'] = 'Reprovado'
+for k, v in aluno.items():
+    print(f'  - {k} é igual a {v}')
