8d7c126862e2dc9a99d529a5ca0978ab5eed75f9
Reinaldo/python <reinaldo_997@hotmail.com>
2025-12-23 11:21:42 -0300

    exercicios python

---
+++ exercicio 97.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('=' * tam)
+    print(f' {msg}')
+    print('=' * tam)
+
+
+#programa principal
+escreva('Gustavo guanabara')
+escreva('Reinaldo ex jogador profissional')
+escreva('kadaki')
