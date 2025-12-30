a563b6643f3bc8e1ac01379361bea326812ca83d
Reinaldo/python <reinaldo_997@hotmail.com>
2025-12-29 22:05:15 -0300

    exercicios python

---
+++ exercicio 105.py
@@ -0,0 +1,26 @@
+def notas(*n, sit=False):
+    """
+    -> Função para analisar notas e situações de vários alunos.
+    :param n: uma ou mais notas dos alunos (aceita varias)
+    :param sit: valor opcional, indicando se deve ou não adicionar a situação
+    :return: dicionario que retorna varias informaçôes sobre a turma.
+    """
+    r = dict()
+    r['total'] = len(n)
+    r['maior'] = max(n)
+    r['menor'] = min(n)
+    r['media'] = sum(n)/len(n)
+    if sit:
+        if r['media'] >= 7:
+            r['situação'] = 'BOA'
+        elif r['media'] >= 5:
+            r['situação'] = 'RAZOAVEL'
+        else:
+            r['situação'] = 'RUIM'
+    return r
+
+
+# Programa principal
+resp = notas(5.5, 6.9, 10, sit=True)
+print(resp)
+help(notas)
