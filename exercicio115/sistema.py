69a24156dddd2eeb62078de092ed51bb5b901b7c
Reinaldo/python <reinaldo_997@hotmail.com>
2026-01-05 08:54:38 -0300

    exercicios python

--- exercicio115/sistema.py
+++ exercicio115/sistema.py
@@ -10,12 +10,19 @@ if not arquivoExiste(arq):
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do sistema'])
     if resposta == 1:
+        # Opção de listar o conteudo de um arquivo.
         lerArquivo(arq)
     elif resposta == 2:
-        cabeçalho ('Opção 2')
+        # Opção de cadastrar uma nova pessoa.
+        cabeçalho ('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt(input('Idade: '))
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
+        # Opção de sair do sistema.
         cabeçalho ('Saindo do sistema... Até logo!')
         break
     else:
+        # Digitou uma opção errada no menu.
         print('\033[31mERRO! Digite uma opção valida!\033[m')
     sleep(1)
